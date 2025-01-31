958255677168892bb00595fe5eb68556516466dd
Evie Ciobanu <me@evie.ro>
2025-01-31 19:17:50 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = ''A garnix module for projects using Node.js.
 
-  See the [module documentation](https://garnix.io/docs/modules/nodejs) or the [module repo](https://github.com/garnix-io/nodejs-module) additional info.
+  Add dependencies, run tests, and optionally deploy a web server.
+
+  [Documentation](https://garnix.io/docs/modules/nodejs) - [Source](https://github.com/garnix-io/nodejs-module).
   '';
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
